dd58116597d2ca82562badb3d6504b9afd780665
Vallinrinne Henri <henri.vallinrinne@hiq.fi>
2018-02-07 16:18:51 +0200

    breakpointit saatu toimimaan styluksella

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 /*import logo from './logo.svg';*/
-import './styles/main.styl';
+import './main.css';
 
 class App extends Component {
   render() {
--- project/src/index.js
+++ project/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/main.styl';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+require("./main.css");
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
