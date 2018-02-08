b443d5d6bac0345176d35d5ef965df4b51fad0cd
Vallinrinne Henri <henri.vallinrinne@hiq.fi>
2018-02-08 15:56:06 +0200

    Näyttökokotilanteet lisätty App.js

--- project/src/App.js
+++ project/src/App.js
@@ -3,14 +3,55 @@ import React, { Component } from 'react';
 import './main.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
   render() {
+
+  const isMobile = this.state.width <= 600;
+  const isTablet = this.state.width <= 800;
+
+  if (isMobile) {
     return (
       <div className="App">
-        asd
+        <h1>Mobile</h1>
           {/*<img src={logo} className="App-logo" alt="logo" />*/}
       </div>
     );
   }
+  if (isTablet) {
+    return (
+      <div className="App">
+        <h1>Tablet</h1>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
+        <h1>Desktop</h1>
+      </div>
+    );
+  }
+}
 }
 
 export default App;
